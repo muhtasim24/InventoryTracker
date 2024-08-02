7b2dda8a61a287bf951e4f578bce484862683570
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-08-02 02:59:28 -0400

    Finished simple UI of add, removing, and items appearing when adding new items

--- app/page.js
+++ app/page.js
@@ -140,12 +140,44 @@ export default function Home() {
           Inventory Items 
         </Typography>
       </Box>
-    </Box>
     <Stack width = "800px" height = "300px" spacing={2} overflow="auto">
       {
-        inventory.map()
-      }
+        inventory.map(({name, quantity}) => (
+          <Box 
+          key = {name} 
+          width="100%"
+          minHeight="150px"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          bgColor="#f0f0f0"
+          padding={5}
+          >
+            <Typography variant="h3" color='#333' textAlign='center'>
+              {name.charAt(0).toUpperCase() + name.slice(1)}
+            </Typography>
+            <Typography variant="h3" color='#333' textAlign='center'>
+              {quantity}
+            </Typography>
+            <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                addItem(name)
+              }}>
+                Add
+              </Button>
+            <Button 
+              variant="contained"
+              onClick={() => {
+                removeItem(name)
+              }}> Remove
+            </Button>
+            </Stack>
+          </Box>
+        ))}
     </Stack>
+    </Box>
   </Box>
   )
 }
