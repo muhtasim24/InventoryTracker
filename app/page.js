7f406262bec8f0ab48ba2a4860064ad6632e1a51
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-08-02 00:17:24 -0400

    TextField on modal and closing modal

--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
-import { Box, Typography } from "@mui/material";
+import { Box, Modal, Typography, Stack, TextField } from "@mui/material";
 import { async } from "@firebase/util";
 import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([]) //state var used to store inventory
-  const [open, setOpen] = useState(false) // state var used to add/remove 
+  const [open, setOpen] = useState(true) // state var used to add/remove 
   const [itemName, setItemName] = useState('') // used to store name of item
 
   // async means it wont block our code when fetching
@@ -70,19 +70,55 @@ export default function Home() {
 
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  
-  return <Box>
+
+  return (
+  <Box 
+    width="100vw" 
+    height="100vh" 
+    display="flex" 
+    justifyContent="center"
+    alignItems = "center"
+    gap={2}
+  >
+    <Modal open={open} onClose={handleClose}>
+      <Box 
+        position="absolute" 
+        top="50%" 
+        left="50%"
+        width={400}
+        bgcolor="white"
+        border="2px solid #000"
+        boxShadow={24}
+        p={4}
+        display="flex"
+        flexDirection="column"
+        gap={3}
+        sx={{
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Typography variant="h6">Add Item</Typography>
+        <Stack width="100%" direction="row" spacing={2}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            value={itemName}
+            onChange={(e) => { // e is the events
+              setItemName(e.target.value)
+            }}
+          />
+          <Button>
+            variant="outlined"
+            onClick={() => {
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}
+          </Button>
+        </Stack>
+      </Box>
+    </Modal>
     <Typography variant="h1"> Inventory Management </Typography>
-    {
-      inventory.forEach((item) => {
-        console.log(item)
-        return (
-          <Box>
-            {item.name}
-            {item.count}
-          </Box>
-        )
-      })
-    }
-    </Box>
+  </Box>
+  )
 }
