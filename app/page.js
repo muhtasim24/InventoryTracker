a86601331e1c5cc9dd2e391cd31cc4cba88da2dc
Muhtasim Mushfiq <muhtasim01@gmail.com>
2024-08-02 00:25:38 -0400

    Button to handle opening the modal

--- app/page.js
+++ app/page.js
@@ -120,7 +120,12 @@ export default function Home() {
         </Stack>
       </Box>
     </Modal>
-    <Typography variant="h1"> Inventory Management </Typography>
+    <Button 
+      variant = "contained" onClick={() => {
+        handleOpen()
+      }}>
+        Add New Item
+    </Button>
   </Box>
   )
 }
